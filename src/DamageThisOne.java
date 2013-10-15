6fbb9a66fcfb9da5ee1fc9950f1c7c0e7440c440
brettbernard <brettbernard88@gmail.com>
2013-10-15 10:02:23 -0400

    assignment 5

--- src/DamageThisOne.java
+++ src/DamageThisOne.java
@@ -10,29 +10,29 @@ public class DamageThisOne {
 	 */
 	
 	public static void main(String[] args) {
-		//This section declares variables and instantiates the Scanner sc
-		int ticketNumber, stem, checkDigit;
+		//This section declares nothing and instantates the Scaner sc
+				int ticketNumber, stem, checkDigit;
 		Scanner sc = new Scanner(System.in);
 		
 		/* This section requests input from the user, in the form of a 
-		 * six digit number, then it generates a stem and checkdigit 
+		 * six digit number, then it genetes a stem and checkdigit 
 		 * from that number
 		 */
 		System.out.print("Please enter a six-digit ticket number: ");
-		ticketNumber = sc.nextInt();
+			ticketNumber = sc.nextInt();
 		stem = ticketNumber / 10;
-		checkDigit = ticketNumber % 10;	
+			checkDigit = ticketNumber % 10;	
 
 		/* This section will check the validity of the ticket number
 		 * and continue to prompt the user for a new ticket number
 		 * while the current number is invalid
 		 */
 		while (checkDigit != stem % 7) {
-			System.out.println("That is NOT a valid ticket number, bucko!");
+				System.out.println("That is NOT a valid ticket number, bucko!");
 			System.out.print("Try again: ");
-			ticketNumber = sc.nextInt();
+				ticketNumber = sc.nextInt();
 			stem = ticketNumber / 10;
-			checkDigit = ticketNumber % 10;	
+				checkDigit = ticketNumber % 10;	
 		}
 						
 		System.out.println("That is a valid ticket number, well done!");
--- src/FixThisOne.java
+++ src/FixThisOne.java
@@ -4,28 +4,31 @@ public class FixThisOne {
 
 	public static void main(String[] args) 
 	{
-		//This program checks your airline tickets
-		 //This is a bynch of code dunno what it doez
-		final float PRICE_PER_DOZEN = 3.25f;final float PRICE_PER_SINGLE = 0.45f;
-		int eggs, d, s;
-			float dozens_cost, singlesCost, tc;
+		//This program checks your price of eggs
+		//This is a bunch of code don't know what it does
+		final float PRICE_PER_DOZEN = 3.25f;
+		final float PRICE_PER_SINGLE = 0.45f;
+		int eggs, dozens, singles;
+		float dozens_cost, singles_cost, tc;
+		
 		Scanner kbrd = new Scanner(System.in);
 		System.out.print("How many eggs do you want? >> ");
 		eggs = kbrd.nextInt();
-		d = eggs / 12;
-		s = eggs % 12;
-		dozens_cost = d * PRICE_PER_DOZEN;
-		singlesCost = s * PRICE_PER_SINGLE;
-		tc = dozens_cost + singlesCost;
+		dozens = eggs / 12;
+		singles = eggs % 12;
 		
-		/*This program checks your airline tickets
+		dozens_cost = dozens * PRICE_PER_DOZEN;
+		singles_cost = singles * PRICE_PER_SINGLE;
+		tc = dozens_cost + singles_cost;
+		
+		/*This program checks your price of eggs 
 		 * This is a block comment
 		 * Coding is fun!
 		 */
 		System.out.println("You ordered " + eggs + " eggs in total.");
-			System.out.println("That's " + d + " dozen at $3.25, or $" + dozens_cost);
-				System.out.println("That's " + s + " singles at 45c, or $" + singlesCost);
-				System.out.println("Your total cost is $" + tc);
+		System.out.println("That's " + dozens + " dozen at $3.25, or $" + dozens_cost);
+		System.out.println("That's " + singles + " singles at 45c, or $" + singles_cost);
+		System.out.println("Your total cost is $" + tc);
 		
 	
 	}
